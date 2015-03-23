a29add5ffd6398156b6c4153df523ea4d19895e2
Jason Tordsen <jason@Jasons-MacBook-Pro.local>
2015-03-23 13:38:57 -0400

    production is default

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  */
 
-define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'local');
+define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
 define('SITE', isset($_SERVER['CI_SITE']) ? $_SERVER['CI_SITE'] : 'cfm');
 
 /*
