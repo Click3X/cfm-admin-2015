ce30c0c2f357d23ca984b22f964c40845777c2b7
Jason Tordsen <jason@Jasons-MacBook-Pro.local>
2015-04-14 15:03:10 -0400

    adding uppercase models

---
+++ application/models/Links_model.php
@@ -0,0 +1,12 @@
+<?php
+
+	class Links_Model extends C3X_Model
+	{
+		public function Links_Model()
+		{
+			$this->table = "links";
+			$this->pk = "id";
+		}
+	}
+
+?>
---
+++ application/models/Project_module_lu_model.php
@@ -0,0 +1,12 @@
+<?php
+
+	class Project_Module_Lu_Model extends C3X_Model
+	{
+		public function Project_Module_Lu_Model()
+		{
+			$this->table = "project_module_lu";
+			$this->pk = "id";
+		}
+	}
+
+?>
